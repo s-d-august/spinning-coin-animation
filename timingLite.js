9a6a78690541acdf76f57b9a39312198310006e2
s-d-august <sinclairaugust@gmail.com>
2024-02-04 09:40:55 -0600

    removed copyCSS function

--- timingLite.js
+++ timingLite.js
@@ -41,20 +41,7 @@ document.querySelector('#about__button').addEventListener('click', () => {
   collapseElement();
 })
 
-function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("css-output");
 
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-} 
 
 var slider = document.getElementById("speedInput");
 var output = document.getElementById("demo");
